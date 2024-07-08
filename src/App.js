dcab5651ff5c6759f0af503368703b1554a67cf9
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-07-08 15:51:37 -0300

    made earth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import { OrbitControls } from "@react-three/drei";
+
+const App = () => {
+  const texture = useLoader(TextureLoader, "earth_texture.jpg");
+
+  const myMesh = React.useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Earth</h1>
+      <div className="canvas-container">
+        <Canvas ref={myMesh}>
+          <mesh>
+            <sphereGeometry args={[2, 40, 40]} />
+            <meshBasicMaterial map={texture} />
+          </mesh>
+
+          <OrbitControls
+            autoRotate={true}
+            dampingFactor={true}
+            maxDistance={8}
+            minDistance={4}
+          />
+        </Canvas>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
